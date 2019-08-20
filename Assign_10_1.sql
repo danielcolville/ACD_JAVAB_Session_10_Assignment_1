3dbe88b48d8c40ccbaa39237ca8c83341fa8614a
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-20 16:37:23 -0400

    Add files via upload

---
+++ Assign_10_1.sql
@@ -0,0 +1,17 @@
+create database bank;
+
+use bank;
+drop table customer;
+CREATE TABLE `bank`.`customer` (
+  `c_id` VARCHAR(10) NOT NULL,
+  `cname` VARCHAR(20) NOT NULL,
+  `mno` INT NULL,
+  `address` VARCHAR(30) NULL,
+  `city` VARCHAR(10) NULL,
+  PRIMARY KEY (`c_id`));
+
+
+alter table `customer`
+add column DOB date;
+alter table `customer`
+CHANGE COLUMN `address` `address` VARCHAR(20) NULL DEFAULT NULL ;
